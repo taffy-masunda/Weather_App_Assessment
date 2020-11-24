d22c582436527e06f2d80bcdc75ebd4ba76ecbb6
Tafadzwa 'Taffy' Masunda <temasunda@gmail.com>
2020-11-24 14:04:28 +0200

    adding onclick for maps and add marker

--- app/src/main/java/za/co/taffy/weatherappassessment/MainActivity.kt
+++ app/src/main/java/za/co/taffy/weatherappassessment/MainActivity.kt
@@ -2,10 +2,34 @@ package za.co.taffy.weatherappassessment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mapFragment : SupportMapFragment
+    lateinit var googleMap : GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupMapView()
+    }
+
+    fun setupMapView() {
+        mapFragment = supportFragmentManager.findFragmentById(R.id.maps_fragment) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+
+            googleMap.setOnMapClickListener { it ->
+                googleMap.clear()
+                googleMap.addMarker(MarkerOptions().position(it))
+                googleMap.animateCamera(CameraUpdateFactory.newLatLng(it))
+            }
+        })
     }
 }
