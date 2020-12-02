3ca965d53f949c88ac70b2c46db25d9f99c73b1a
Tafadzwa 'Taffy' Masunda <temasunda@gmail.com>
2020-12-02 13:36:05 +0200

    Adding search functionality to the main map to search for a single location, add market to the map, save it to the local database.

--- app/src/main/java/za/co/taffy/weatherappassessment/ui/MainActivity.kt
+++ app/src/main/java/za/co/taffy/weatherappassessment/ui/MainActivity.kt
@@ -1,20 +1,25 @@
 package za.co.taffy.weatherappassessment.ui
 
 import android.content.Intent
+import android.location.Address
 import android.location.Geocoder
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import za.co.taffy.weatherappassessment.R
 import za.co.taffy.weatherappassessment.database.SavedLocation
 import za.co.taffy.weatherappassessment.presentation.DatabasePresenter
+import java.io.IOException
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -24,6 +29,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var savedLocationsIcon: ImageView
     lateinit var geocoder: Geocoder
     lateinit var presenter: DatabasePresenter
+    lateinit var searchView : SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,15 +41,58 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     fun setupViews(): Unit {
-        mapFragment =
-            supportFragmentManager.findFragmentById(R.id.maps_fragment) as SupportMapFragment
+        mapFragment = supportFragmentManager.findFragmentById(R.id.maps_fragment) as SupportMapFragment
         settingsIcon = findViewById(R.id.setting_icon_imageview)
         savedLocationsIcon = findViewById(R.id.saved_locations_icon_imageview)
+        searchView = findViewById(R.id.map_searchview)
     }
 
     private fun setupOnclicks() {
         savedLocationsIcon.setOnClickListener(this)
         settingsIcon.setOnClickListener(this)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                googleMap.clear()
+
+                val location= searchView.query.toString()
+                lateinit var addressList : List<Address>
+
+                if(location != ""){
+                    geocoder = Geocoder(baseContext)
+                     try {
+                         addressList = geocoder.getFromLocationName(location, 1)
+                         var address = addressList.get(0)
+
+                         var locationLatLong = LatLng(address.latitude, address.longitude)
+                         val mapMarker = googleMap.addMarker(
+                             MarkerOptions()
+                                 .position(locationLatLong)
+                                 .title(location)
+                                 .snippet("Click for Forecast")
+                         )
+                         mapMarker.showInfoWindow()
+                         googleMap.animateCamera(CameraUpdateFactory.newLatLng(locationLatLong))
+
+                         googleMap.setOnInfoWindowClickListener(GoogleMap.OnInfoWindowClickListener {
+                             openWeatherResultsActivity(locationLatLong.latitude, locationLatLong.longitude)
+                         })
+
+                         saveLocationToRoomDatabase(location, locationLatLong.latitude, locationLatLong.longitude)
+
+                     }catch (io: IOException){
+                         Log.d(constants.TAG, "Error finding address by location name : \n{$io}")
+                     }
+                }
+
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
     }
 
     fun setupMapView() {
@@ -120,4 +169,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    object constants {
+        const val TAG = "SEARCH TAG"
+    }
 }
